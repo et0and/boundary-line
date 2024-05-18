65c1d563fbacd9e5f9a69bd872942859621183da
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-18 04:18:50 +0000

    Import issue

--- boundary-line/src/pages/index.tsx
+++ boundary-line/src/pages/index.tsx
@@ -1,6 +1,6 @@
 
 import { Inter } from "next/font/google";
-import Marquee from '../components/Marquee';
+import Marquee from '../components/marquee';
 
 const inter = Inter({ subsets: ["latin"] });
 
